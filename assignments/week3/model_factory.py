d1a727c36e2febd59085b7d1b5a041566adc3125
jiawenzhang0823 <jiawen22@seas.upenn.edu>
2023-02-04 11:23:39 -0500

    update

--- assignments/week3/model_factory.py
+++ assignments/week3/model_factory.py
@@ -16,5 +16,5 @@ def create_model(input_dim: int, output_dim: int) -> MLP:
 
     """
     return MLP(
-        input_dim, 256, output_dim, 2, torch.nn.ReLU, torch.nn.init.kaiming_normal
+        input_dim, 256, output_dim, 2, torch.nn.LeakyReLU, torch.nn.init.kaiming_normal
     )
