359f3f056ad251646355c8fe9c5ac56e99dfd7d1
jiawenzhang0823 <jiawen22@seas.upenn.edu>
2023-03-18 12:10:01 -0400

    update

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -6,15 +6,13 @@ from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 32
-    num_epochs = 12
+    batch_size = 128
+    num_epochs = 3
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
     transforms = Compose(
-        [ToTensor(), Normalize(mean=[0.5,0.5,0.5], std=[0.5,0.5,0.5])
-            #, Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])
-         ]
+        [ToTensor(), Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])]
     )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -13,9 +13,7 @@ class Model(torch.nn.Module):
         Initialize the model
         """
         super().__init__()
-        self.conv_layer1 = nn.Conv2d(
-            in_channels=num_channels, out_channels=16, kernel_size=3, stride=2
-        )
+        self.conv_layer1 = nn.Conv2d(num_channels, 16, 3, 2)
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.batchnorm = nn.BatchNorm2d(16)
         self.fc1 = nn.Linear(784, 35)
