347033628371dce6a79ee6db4fdfb00aeef44996
jiawenzhang0823 <jiawen22@seas.upenn.edu>
2023-03-18 13:04:06 -0400

    update

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+torch.manual_seed(1234)
 
 class Model(torch.nn.Module):
     """
@@ -13,11 +13,11 @@ class Model(torch.nn.Module):
         Initialize the model
         """
         super().__init__()
-        self.conv_layer1 = nn.Conv2d(num_channels, 16, 5, 2)
+        self.conv_layer1 = nn.Conv2d(num_channels, 16, 4, 2)
         self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.batchnorm = nn.BatchNorm2d(16)
-        self.fc1 = nn.Linear(784, 30)
-        self.fc2 = nn.Linear(30, num_classes)
+        self.fc1 = nn.Linear(784, 35)
+        self.fc2 = nn.Linear(35, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
