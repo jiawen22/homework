102675afd605dc781e9e12c8bb66469efd1ac11a
jiawenzhang0823 <jiawen22@seas.upenn.edu>
2023-03-18 10:52:41 -0400

    update

--- assignments/04-Convs/config.py
+++ assignments/04-Convs/config.py
@@ -2,15 +2,17 @@ from typing import Callable
 import torch
 import torch.optim
 import torch.nn as nn
-from torchvision.transforms import Compose, ToTensor
+from torchvision.transforms import Compose, ToTensor, Normalize
 
 
 class CONFIG:
-    batch_size = 8
-    num_epochs = 8
+    batch_size = 32
+    num_epochs = 12
 
     optimizer_factory: Callable[
         [nn.Module], torch.optim.Optimizer
     ] = lambda model: torch.optim.Adam(model.parameters(), lr=1e-3)
 
-    transforms = Compose([ToTensor()])
+    transforms = Compose(
+        [ToTensor(), Normalize(mean=[0.485, 0.456, 0.4], std=[0.229, 0.224, 0.2])]
+    )
--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,39 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """
+    The CNN model
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        """
+        Initialize the model
+        """
+        super().__init__()
+        self.conv_layer1 = nn.Conv2d(
+            in_channels=num_channels, out_channels=16, kernel_size=3
+        )
+        self.conv_layer2 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=3)
+        self.max_pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.batchnorm = nn.BatchNorm2d(16)
+        self.fc = nn.Linear(576, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        Train the model
+        Return:
+            Output: torch.Tensor
+        """
+        out = F.relu(self.conv_layer1(x))
+        out = self.batchnorm(out)
+        out = self.max_pool(out)
+        out = F.relu(self.conv_layer2(out))
+        out = self.max_pool(out)
+
+        out = out.reshape(out.size(0), -1)
+        out = self.fc(out)
+
+        return out
